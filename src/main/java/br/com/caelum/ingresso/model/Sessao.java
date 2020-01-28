3fcd22f305e2728c722d681d72723f197231e508
Victor Balbino <victorbm00@outlook.pt>
2020-01-28 12:48:59 -0300

    colocando mais informações no README.md

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,13 +2,34 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
-public class Sessao {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+
+@Entity
+public class Sessao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
+	 /**
+     * @deprecated hibernate only
+     */
+    public Sessao() {
+
+    }
+    
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
@@ -46,5 +67,4 @@ public class Sessao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
 }
